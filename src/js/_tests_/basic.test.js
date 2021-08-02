eff3048ad116d3973e75e1ba331f2548c5a7224a
Maria <mariaes2016@yandex.ru>
2021-08-02 19:21:50 +0300

    unit-test

--- src/js/_tests_/basic.test.js
+++ src/js/_tests_/basic.test.js
@@ -1,7 +1,47 @@
-import sum from '../basic';
+import showHealth from '../health';
 
-test('should sum', () => {
-  const result = sum([1, 2, 3]);
-
-  expect(result).toBe(6);
+test('should healthy', () => {
+  const received = showHealth({ name: 'Маг', health: 90 });
+  expect(received).toBe('healthy');
 });
+
+test('should wounded', () => {
+  const received = showHealth({ name: 'Маг', health: 40 });
+  expect(received).toBe('wounded');
+});
+
+test('should critical', () => {
+  const received = showHealth({ name: 'Маг', health: 10 });
+  expect(received).toBe('critical');
+});
+
+test('Parameter is not a number!', () => {
+  expect(() => {
+    showHealth({});
+  }).toThrow();
+});
+
+test('Parameter is not a number!', () => {
+  expect(() => {
+    showHealth({ name: 'Маг', health: 'mistake' });
+  }).toThrow();
+});
+
+test('Parameter is not a number!', () => {
+  expect(() => {
+    showHealth({ name: 'Маг', health: -10 });
+  }).toThrow();
+});
+
+/* test('should killed', () => {
+  const received = showHealth({ name: 'Маг', health: 'mistake' });
+  expect(received).toBe('killed');
+});
+test('should killed', () => {
+  const received = showHealth({});
+  expect(received).toBe('killed');
+});
+test('should killed', () => {
+  const received = showHealth({ name: 'Маг', health: -10 });
+  expect(received).toBe('killed');
+}); */
